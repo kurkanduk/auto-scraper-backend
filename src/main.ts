643335808e8e77441cbc9d649a66e01f843282f0
kurkanduk <vovasuperman522@gmail.com>
2025-08-18 15:59:43 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS with options
   app.enableCors({
-    origin: ['http://localhost:5173'], // your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // if you need to send cookies
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: [
+      'https://auto-bot-frontend.vercel.app',
+      'http://localhost:5173', // если тестишь локально
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
   await app.listen(process.env.PORT || 3000);
