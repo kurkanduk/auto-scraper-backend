a0f811d13bc4aed707ce9cdf15057c9cd3ac210b
kurkanduk <vovasuperman522@gmail.com>
2025-08-18 21:03:06 +0200

    a

--- src/modules/whatsapp/whatsapp.service.ts
+++ src/modules/whatsapp/whatsapp.service.ts
@@ -44,11 +44,6 @@ export class WhatsappService implements OnModuleInit {
   }
 
   private async initializeClient() {
-    const executablePath =
-      process.env.NODE_ENV === 'production'
-        ? await chromium.executablePath // use chrome-aws-lambda on Heroku
-        : undefined; // let Puppeteer find local Chrome in dev
-
     this.client = new Client({
       authStrategy: new LocalAuth({
         clientId: 'car-scraper-bot',
@@ -56,9 +51,7 @@ export class WhatsappService implements OnModuleInit {
       puppeteer: {
         headless: true,
         timeout: 60000,
-        executablePath, // 👈 important
         args: [
-          ...chromium.args, // 👈 recommended args for AWS/Heroku
           '--no-sandbox',
           '--disable-setuid-sandbox',
           '--disable-dev-shm-usage',
